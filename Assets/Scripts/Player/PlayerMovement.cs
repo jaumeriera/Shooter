0d0b889e5ed359844c442fbdb4945d10180cdc52
Jaume Riera <jaumerg95@gmail.com>
2023-03-23 02:40:08 +0100

    Implement run

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,14 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
     [SerializeField] private float speed = 12f;
+    [SerializeField] private float sprintBoost = 8f;
 
+
+    float currentSpeed;
     void Update()
     {
         float xMove = Input.GetAxis("Horizontal");
         float zMove = Input.GetAxis("Vertical");
-
+        
         Vector3 move = transform.right * xMove + transform.forward * zMove;
 
-        controller.Move(move * speed * Time.deltaTime);
+        if (Input.GetButton("Run")) {
+            controller.Move(move * (speed + sprintBoost) * Time.deltaTime);
+        } else {
+            controller.Move(move * speed * Time.deltaTime);
+        }
     }
 }
